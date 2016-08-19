9dce5a366e1d968750807fb8d3df65d4f963680a
Jarrett <jbacey@gmail.com>
2016-08-19 13:03:32 -0400

    updated rpnCalc

--- Day4/RPNCalculator.java
+++ Day4/RPNCalculator.java
@@ -4,35 +4,9 @@ import java.util.Stack;
 
 public class RPNCalculator {
 
-/*	int nbr1 = 8;
-	int nbr2 = 5;
-	int result = nbr1 + nbr2;
-	char minus = '-';
-	char plus = '+';
-	char multiply = '*';
-	char divide = '/';*/
-
-	
-	
-
-
-
-//	static void showpush(Stack st, int a) {
-//	      st.push(new Integer(a));
-//	      System.out.println("push(" + a + ")");
-//	      System.out.println("stack: " + st);
-//	   }
-//
-//	static void showpop(Stack st) {
-//	      System.out.print("pop -> ");
-//	      Integer a = (Integer) st.pop();
-//	      System.out.println(a);
-//	      System.out.println("stack: " + st);
-//	}
-
 
 	public static void main(String[] args) {
-		char[] problem = new char[] { '8', '5', '+' };
+		char[] problem = new char[] { '8', '2', '/', '9', '*', '1', '-', '7', '/' };
 		Stack<Integer> stk = new Stack<Integer>();
 
 		for (char ch:problem) {
@@ -40,45 +14,27 @@ public class RPNCalculator {
 			if (nbr != -1) {
 				stk.push(nbr);
 			} else {
-				if (ch == '+') {
-					int p1 = stk.pop();
-					int p2 = stk.pop();
-					int pTotal = p1 + p2;
-					stk.push(pTotal);
-				}
-			} //if 
-		}
+				int p1 = stk.pop();
+				int p2 = stk.pop();
+				int pSolution;
+				if (ch == '*') {
+					pSolution = p1 * p2;
+					stk.push(pSolution);
+				} else if (ch == '/') {
+					pSolution = p2 / p1;
+					stk.push(pSolution);
+				} else if (ch == '+') {
+					pSolution = p1 + p2;
+					stk.push(pSolution);
+				} else if (ch == '-') {
+					pSolution = p2 - p1;
+					stk.push(pSolution);
+				} 				
+			} // end of if	
+		} // end of for
 		int ans = stk.pop();
-	      System.out.printf("Calculating 8,5+. Answer is %d",ans);
-
+	      System.out.printf("Answer is %d\n",ans);
+	} // end of main
+}
 		
 		
-		
-		
-//		for(int i = 0; i < problem.length;i++) {
-//			int k = (problem.length-1)-i;
-//			showpush(stk , problem[k]);
-		}
-		
-		
-		
-		}
-
-		
-	
-
-
-	 
-	  	//int anInt = (int) stk.pop();
-
-	      
-	      
-	      
-	      
-	      
-	      
-	      
-	      
-	      
-	 
-
