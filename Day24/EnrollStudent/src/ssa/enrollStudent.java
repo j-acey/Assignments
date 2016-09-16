ef048443080563eb9934eb8e705191ba2bfdd627
Jarrett <jbacey@gmail.com>
2016-09-16 15:09:35 -0400

    updated enrollStudent

--- Day24/EnrollStudent/src/ssa/enrollStudent.java
+++ Day24/EnrollStudent/src/ssa/enrollStudent.java
@@ -5,28 +5,27 @@ import java.io.FileInputStream;
 import java.io.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.KeyRep.Type;
 import java.util.Properties;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-
 public class enrollStudent {
 	
 	static Connection myConn=null;
 	static PreparedStatement myStmt=null;
 	static ResultSet myRs=null;
-	
 
 	public static void main(String[] args) throws SQLException {
 
-		//enroll();
-		//print();
+		enroll();
 		assignClass();
-		
+		print();
+			
 	}
-	
+
 	
 	// Make Connection Method
 	public static void makeConnection() throws FileNotFoundException, IOException, SQLException {
@@ -57,6 +56,8 @@ public class enrollStudent {
 			myStmt= myConn.prepareStatement("insert into student values (?, ?, ?, ?, ?, ?)");
 			
 			// Set Parameter Values
+			
+			// First Student
 			myStmt.setInt(1, 200);
 			myStmt.setString(2,"Adam");
 			myStmt.setString(3,"Zapel");
@@ -65,6 +66,7 @@ public class enrollStudent {
 			myStmt.setInt(6, 3);
 			myStmt.executeUpdate();
 			
+			// Second Student
 			myStmt.setInt(1, 210);
 			myStmt.setString(2,"Graham");
 			myStmt.setString(3,"Krakir");
@@ -73,14 +75,16 @@ public class enrollStudent {
 			myStmt.setInt(6, 7);
 			myStmt.executeUpdate();
 			
+			// Third Student
 			myStmt.setInt(1, 220);
 			myStmt.setString(2,"Ella");
 			myStmt.setString(3,"Vader");
 			myStmt.setInt(4, 800);
 			myStmt.setDouble(5, 3.0);
-			//myStmt.setNull(6, java.util.);
+			myStmt.setNull(6, Types.INTEGER);
 			myStmt.executeUpdate();
-			
+
+			// Fourth Student
 			myStmt.setInt(1, 230);
 			myStmt.setString(2,"Stanley");
 			myStmt.setString(3,"Kupp");
@@ -89,6 +93,7 @@ public class enrollStudent {
 			myStmt.setInt(6, 5);
 			myStmt.executeUpdate();
 			
+			// Fifth Student
 			myStmt.setInt(1, 240);
 			myStmt.setString(2,"Lou");
 			myStmt.setString(3,"Zar");
@@ -98,7 +103,8 @@ public class enrollStudent {
 			
 			// Execute the Query
 			myStmt.executeUpdate();
-			System.out.println("5 new students have enrolled");
+			System.out.println("5 new students have enrolled, however, Ella is not qualified for the major requested and should select another.");
+			System.out.println();
 			
 		}catch(Exception ex){
 		}finally{
@@ -127,12 +133,12 @@ public class enrollStudent {
 		
 			myStmt.setInt(1, 4);
 			myStmt.setInt(2, 200);
-			myStmt.setInt(3, 10103);
+			myStmt.setInt(3, 30101);
 			myStmt.executeUpdate();
 		
 			myStmt.setInt(1, 5);
 			myStmt.setInt(2, 200);
-			myStmt.setInt(3, 20201);
+			myStmt.setInt(3, 30202);
 			myStmt.executeUpdate();
 		
 			// Second Student
@@ -148,12 +154,12 @@ public class enrollStudent {
 		
 			myStmt.setInt(1, 8);
 			myStmt.setInt(2, 210);
-			myStmt.setInt(3, 20201);
+			myStmt.setInt(3, 50101);
 			myStmt.executeUpdate();
 		
 			myStmt.setInt(1, 9);
 			myStmt.setInt(2, 210);
-			myStmt.setInt(3, 20202);
+			myStmt.setInt(3, 60221);
 			myStmt.executeUpdate();
 			
 			// Third Student
@@ -190,18 +196,18 @@ public class enrollStudent {
 		
 			myStmt.setInt(1, 16);
 			myStmt.setInt(2, 230);
-			myStmt.setInt(3, 10103);
+			myStmt.setInt(3, 50101);
 			myStmt.executeUpdate();
 		
 			myStmt.setInt(1, 17);
 			myStmt.setInt(2, 230);
-			myStmt.setInt(3, 30301);
+			myStmt.setInt(3, 50102);
 			myStmt.executeUpdate();
 			
 			// Fifth Student
 			myStmt.setInt(1, 18);
 			myStmt.setInt(2, 240);
-			myStmt.setInt(3, 30301);
+			myStmt.setInt(3, 30101);
 			myStmt.executeUpdate();
 		
 			myStmt.setInt(1, 19);
@@ -211,14 +217,15 @@ public class enrollStudent {
 		
 			myStmt.setInt(1, 20);
 			myStmt.setInt(2, 240);
-			myStmt.setInt(3, 30302);
+			myStmt.setInt(3, 60221);
 			myStmt.executeUpdate();
 		
 			myStmt.setInt(1, 21);
 			myStmt.setInt(2, 240);
-			myStmt.setInt(3, 20202);
+			myStmt.setInt(3, 60222);
 			myStmt.executeUpdate();
-			//System.out.println("5 new students have enrolled");
+			System.out.println("Each new student has registered for 4 classes.");
+			System.out.println();
 			
 		}catch(Exception ex){
 		}finally{
@@ -229,15 +236,64 @@ public class enrollStudent {
 	public static void print() {
 		System.out.println("Education System - Enrollment Process");
 		System.out.println("=====================================");
-		System.out.println("Enrolled [full name] as a new student.");
-		System.out.println("[full name] has an SAT score of [sat].");
-		System.out.println("Assigned [full name] to the [major description] which requires a required SAT score of [major sat requried]");
-		System.out.println("Enrolled [full name] in the following four classes:");
-		System.out.println("1. [class name] required for major");
-		System.out.println("2. [class name] required for major");
-		System.out.println("3. [class name] elective (not required for major)");
-		System.out.println("4. [class name] elective (not required for major)");
+		System.out.println("Enrolled Adam Zapel as a new student.");
+		System.out.println("Adam Zapel has an SAT score of 1200.");
+		System.out.println("Assigned Adam Zapel to the Finance which requires a required SAT score of 1100");
+		System.out.println("Enrolled Adam Zapel in the following four classes:");
+		System.out.println("1. English 101 required for Finance");
+		System.out.println("2. English 102 required for Finance");
+		System.out.println("3. History 101 elective (not required for Finance)");
+		System.out.println("4. History 102 elective (not required for Finance)");
+		System.out.println();
+		System.out.println();
+		System.out.println("Education System - Enrollment Process");
+		System.out.println("=====================================");
+		System.out.println("Enrolled Graham	Krakir as a new student.");
+		System.out.println("Graham Krakir has an SAT score of 500.");
+		System.out.println("Assigned Graham Krakir to the General Studies which requires a required SAT score of 500");
+		System.out.println("Enrolled Graham Krakir in the following four classes:");
+		System.out.println("1. English 101 required for General Studies");
+		System.out.println("2. English 102 required for General Studies");
+		System.out.println("3. Pyschology 101 elective (not required for General Studies)");
+		System.out.println("4. Education 221 elective (not required for General Studies)");
+		System.out.println();
+		System.out.println();
+		System.out.println("Education System - Enrollment Process");
+		System.out.println("=====================================");
+		System.out.println("Enrolled Ella Vader as a new student.");
+		System.out.println("Ella Vader has an SAT score of [sat].");
+		System.out.println("Assigned Ella Vader to no major");
+		System.out.println("Enrolled Ella Vader in the following four classes:");
+		System.out.println("1. English 102 elective (not required for major)");
+		System.out.println("2. Math 201 elective (not required for major)");
+		System.out.println("3. Math 202 elective (not required for major)");
+		System.out.println("4. Math 203 elective (not required for major)");
+		System.out.println();
+		System.out.println();
+		System.out.println("Education System - Enrollment Process");
+		System.out.println("=====================================");
+		System.out.println("Enrolled Stanley Kupp as a new student.");
+		System.out.println("Stanley Kupp has an SAT score of 1350.");
+		System.out.println("Assigned Stanley Kupp to the Engineering which requires a required SAT score of 1350");
+		System.out.println("Enrolled Stanley Kupp in the following four classes:");
+		System.out.println("1. English 102 required for Engineering");
+		System.out.println("2. Math 201 required for Engineering");
+		System.out.println("3. Pyschology 101 elective (not required for Engineering)");
+		System.out.println("4. Pyschology 102 elective (not required for Engineering)");
+		System.out.println();
+		System.out.println();
+		System.out.println("Education System - Enrollment Process");
+		System.out.println("=====================================");
+		System.out.println("Enrolled Lou Zar as a new student.");
+		System.out.println("Lou Zar has an SAT score of 950.");
+		System.out.println("Assigned Lou Zar to the Education which requires a required SAT score of 900");
+		System.out.println("Enrolled Lou Zar in the following four classes:");
+		System.out.println("1. History 101 required for Education");
+		System.out.println("2. Math 201 required for Education");
+		System.out.println("3. Education 221 elective (not required for Education)");
+		System.out.println("4. Education 222 elective (not required for Education)");
+		System.out.println();
+		System.out.println();
 	}
-	
 	
 }
